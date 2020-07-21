cab85ede3c56526a228c01fd796579013949a7af
Thomas Gibson <gibsonthomas1120@hotmail.com>
2020-07-21 16:54:30 -0700

    Organize unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
-using TimeMachine, Test
+using TimeMachine, Test, MPI
 
-# include("ode_tests_basic.jl")
-# include("ode_tests_convergence.jl")
-# include("genericcb_tests.jl")
+include(joinpath("testhelper.jl"))
+
+@testset "ODE Tests: Basic" begin
+    runmpi(joinpath(@__DIR__, "ode_tests_basic.jl"))
+end
+
+# FIXME: Should consolodate all convergence tests into single
+# testset --- this test is slightly redundant
+# @testset "ODE Tests: Convergence" begin
+#     runmpi(joinpath(@__DIR__, "ode_tests_convergence.jl"))
+# end

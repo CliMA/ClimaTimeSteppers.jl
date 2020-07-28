f4e4b3f485c34a7ad668996b271af86b58761399
Simon Byrne <simonbyrne@gmail.com>
2020-07-27 22:41:21 -0700

    get working

--- test/problems.jl
+++ test/problems.jl
@@ -45,7 +45,7 @@ function sincos_sol(u0,p,t)
 end
 
 """
-IMEX problem with constant linear part
+IMEX problem with autonomous linear part
 
 ```math
 \\frac{du}{dt} = u + cos(t)/α
@@ -55,14 +55,24 @@ with initial condition ``u_0 = 1/2``, parameter ``α=4``, and solution
 u(t) = \frac{e^t + sin(t) - cos(t)}{2 α} + u_0 e^t
 ```
 """    
-const imex_linconst_prob = SplitODEProblem(
-    (du, u, p, t) -> (du .= u),
-    (du, u, p, t) -> (du .= cos(t)/p),
+const imex_autonomous_prob = SplitODEProblem(
+    (du, u, p, t, α=true, β=false) -> (du .= α .* u        .+ β .* du),
+    (du, u, p, t, α=true, β=false) -> (du .= α .* cos(t)/p .+ β .* du),
     [0.5], (0.0,1.0), 4.0)
 
-function imex_linconst_sol(u0,p,t)
+function imex_autonomous_sol(u0,p,t)
     (exp(t)  + sin(t) - cos(t)) / 2p .+ exp(t) .* u0
 end
+
+const imex_nonautonomous_prob = SplitODEProblem(
+    (du, u, p, t, α=true, β=false) -> (du .= α .* cos(t) * u .+ β .* du),
+    (du, u, p, t, α=true, β=false) -> (du .= α .* cos(t)/p   .+ β .* du),
+    [0.5], (0.0,2.0), 4.0)
+
+function imex_nonautonomous_sol(u0,p,t)
+    (exp(sin(t)) .* (1 .+ p .* u0) .- 1) ./ p
+end
+
 
 """ 
 Test problem (4.2) from RobertsSarsharSandu2018arxiv
@@ -109,12 +119,14 @@ end
 
 function kpr_fast!(dQ, Q, param, t, α=1, β=0)
     dQf,_ = kpr_rhs(Q,param,t)
-    dQ .= α .* [dQf, 0] .+ β .* dQ
+    dQ[1] = α*dQf + β*dQ[1]
+    dQ[2] = β*dQ[2]
 end
 
 function kpr_slow!(dQ, Q, param, t, α=1, β=0)
     _,dQs = kpr_rhs(Q,param,t)
-    dQ .= α .* [0, dQs] .+ β .* dQ
+    dQ[1] = β*dQ[1]
+    dQ[2] = α*dQs + β*dQ[2]
 end
 
 function kpr_sol(u0,param,t)
@@ -127,8 +139,8 @@ function kpr_sol(u0,param,t)
 end
 
 const kpr_multirate_prob = SplitODEProblem(
-    IncrementingODEFunction(kpr_fast!),
-    IncrementingODEFunction(kpr_slow!),
+    IncrementingODEFunction{true}(kpr_fast!),
+    IncrementingODEFunction{true}(kpr_slow!),
     [sqrt(4), sqrt(3)], (0.0, 5π/2), kpr_param,
 )
 
---
+++ test/utils.jl
@@ -0,0 +1,50 @@
+"""
+    convergence_rates(problem, solution, method, dts; kwargs...)
+
+Compute the errors rates of `method` on `problem` by comparing to `solution`
+on the set of `dt` values in `dts`. Extra `kwargs` are passed to `solve`
+
+`solution` should be a function with a method `solution(u0, p, t)`.
+"""
+function convergence_errors(prob, sol, method, dts; kwargs...)
+  errs = map(dts) do dt
+       # copy the problem so we don't mutate u0
+      prob_copy = deepcopy(prob)
+      u = solve(prob_copy, method; dt=dt, kwargs...)
+      norm(u .- sol(prob.u0, prob.p, prob.tspan[end]))
+  end
+  return errs
+end
+
+
+"""
+  convergence_order(problem, solution, method, dts; kwargs...)
+
+Estimate the order of the rate of convergence of `method` on `problem` by comparing to
+`solution` the set of `dt` values in `dts`.
+
+`solution` should be a function with a method `solution(u0, p, t)`.
+"""
+function convergence_order(prob, sol, method, dts; kwargs...)
+  errs = convergence_errors(prob, sol, method, dts; kwargs...)
+  # find slope coefficient in log scale
+  _,order_est = hcat(ones(length(dts)), log2.(dts)) \ log2.(errs)
+  return order_est
+end
+
+
+
+"""
+    DirectSolver
+
+A linear solver which forms the full matrix of a linear operator and its LU factorization.
+"""
+struct DirectSolver end
+
+DirectSolver(args...) = DirectSolver()
+
+function (::DirectSolver)(x,A,b,matrix_updated; kwargs...)
+  n = length(x)
+  M = mapslices(y -> mul!(similar(y), A, y), Matrix{eltype(x)}(I,n,n), dims=1)
+  x .= M \ b 
+end

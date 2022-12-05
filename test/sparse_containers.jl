c5c53ee37e950384b4cac1b9c11335e76609192a
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-12-05 14:52:17 -0800

    Allow SparseContainers to work with Arrays

--- test/sparse_containers.jl
+++ test/sparse_containers.jl
@@ -19,4 +19,24 @@ using Test
 
     @test_throws ErrorException("No index 2 found in sparse index map (1, 3, 5, 7)") v[2]
     @test_throws ErrorException("No index 8 found in sparse index map (1, 3, 5, 7)") v[8]
+    @inferred v[7]
+
+    a1 = ones(3) .* 1
+    a2 = ones(3) .* 2
+    a3 = ones(3) .* 3
+    a4 = ones(3) .* 4
+    v = SparseContainer([a1,a2,a3,a4], (1,3,5,7))
+    @test v[1] == ones(3) .* 1
+    @test v[3] == ones(3) .* 2
+    @test v[5] == ones(3) .* 3
+    @test v[7] == ones(3) .* 4
+
+    @test parent(v)[1] == ones(3) .* 1
+    @test parent(v)[2] == ones(3) .* 2
+    @test parent(v)[3] == ones(3) .* 3
+    @test parent(v)[4] == ones(3) .* 4
+
+    @test_throws ErrorException("No index 2 found in sparse index map (1, 3, 5, 7)") v[2]
+    @test_throws ErrorException("No index 8 found in sparse index map (1, 3, 5, 7)") v[8]
+    @inferred v[7]
 end

8981b23184242e147e66aa761ef44dd93c092b53
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-12-06 09:44:45 -0800

    Remove test helper

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,6 @@ else
 end
 
 @safetestset "SparseContainers" begin include("sparse_containers.jl") end
-include("testhelper.jl")
 include("problems.jl")
 include("utils.jl")
 
@@ -15,18 +14,7 @@ include("integrator.jl")
 include("convergence.jl")
 include("callbacks.jl")
 include("test_convergence_checker.jl")
-include("test_newtons_method.jl")
-@safetestset "Single column ARS" begin include("single_column_ARS_test.jl") end
 include("compare_generated.jl") # TODO: Remove this.
+@safetestset "Newtons method" begin include("test_newtons_method.jl") end
+@safetestset "Single column ARS" begin include("single_column_ARS_test.jl") end
 @safetestset "Aqua" begin include("aqua.jl") end
-
-#=
-@testset "ODE Tests: Basic" begin
-    runmpi(joinpath(@__DIR__, "basic.jl"))
-end
-=#
-# FIXME: Should consolodate all convergence tests into single
-# testset --- this test is slightly redundant
-# @testset "ODE Tests: Convergence" begin
-#     runmpi(joinpath(@__DIR__, "ode_tests_convergence.jl"))
-# end

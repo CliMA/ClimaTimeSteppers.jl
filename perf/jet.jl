98316c2822bced7f6e46a465637fc3417af886ee
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-12-15 22:03:43 -0800

    Fix jet script

--- perf/jet.jl
+++ perf/jet.jl
@@ -15,7 +15,7 @@ end
 cts = joinpath(dirname(@__DIR__));
 include(joinpath(cts, "test", "problems.jl"))
 function config_integrators(problem)
-    algorithm = CTS.IMEXARKAlgorithm(ARS343(), NewtonsMethod(; linsolve = linsolve_direct, max_iters = 2))
+    algorithm = CTS.IMEXARKAlgorithm(ARS343(), NewtonsMethod(; max_iters = 2))
     dt = 0.01
     integrator = DiffEqBase.init(problem, algorithm; dt)
     integrator.cache = CTS.cache(problem, algorithm)

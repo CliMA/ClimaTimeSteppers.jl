4d1abeaa5847db998fca0358533ba13f00ab09fe
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-12-30 08:51:29 -0800

    Always use maxiter = 2

--- test/utils.jl
+++ test/utils.jl
@@ -4,10 +4,9 @@ using Test
 problem(test_case, tab::CTS.AbstractIMEXARKTableau) = test_case.split_prob
 problem(test_case, tab) = test_case.prob
 
-function algorithm(test_case, tab)
+function algorithm(tab)
     return if tab isa CTS.AbstractIMEXARKTableau
-        max_iters = test_case.linear_implicit ? 1 : 2 # TODO: is 2 enough?
-        CTS.IMEXARKAlgorithm(tab, NewtonsMethod(; max_iters))
+        CTS.IMEXARKAlgorithm(tab, NewtonsMethod(; max_iters = 2))
     else
         tab
     end

c9d29d361bf65021776f53934e2735146107803d
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-09-14 12:28:21 -0700

    colors

--- src/solvers/hard_coded_ars343.jl
+++ src/solvers/hard_coded_ars343.jl
@@ -11,7 +11,7 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
     if !isnothing(newtons_method_cache)
         jacobian = newtons_method_cache.j
         if (!isnothing(jacobian)) && needs_update!(newtons_method.update_j, NewTimeStep(t))
-            NVTX.@range "Wfact" color = colorant"yellow" begin
+            NVTX.@range "Wfact" color = colorant"orange" begin
                 T_imp!.Wfact(jacobian, u, p, dt * γ, t)
             end
         end
@@ -19,57 +19,57 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
 
     s = 4
 
-    NVTX.@range "Stage 1" color = colorant"yellow" begin
+    NVTX.@range "Stage 1" color = colorant"brown" begin
         i::Int = 1
         t_exp = t
-        NVTX.@range "U[i] = u" color = colorant"yellow" begin
+        NVTX.@range "U[i] = u" begin
             @. U[i] = u
         end
-        NVTX.@range "lim!" color = colorant"yellow" begin
+        NVTX.@range "lim!" color = colorant"blue" begin
             lim!(U[i], p, t_exp, u)
         end
-        NVTX.@range "dss!" color = colorant"yellow" begin
+        NVTX.@range "dss!" color = colorant"green" begin
             dss!(U[i], p, t_exp)
         end
-        NVTX.@range "T_lim!" color = colorant"yellow" begin
+        NVTX.@range "T_lim!" color = colorant"red" begin
             T_lim!(T_lim[i], U[i], p, t_exp)
         end
-        NVTX.@range "T_exp!" color = colorant"yellow" begin
+        NVTX.@range "T_exp!" color = colorant"purple" begin
             T_exp!(T_exp[i], U[i], p, t_exp)
         end
 
         i = 2
         t_exp = t + dt * c_exp[i]
-        NVTX.@range "U=u+dt*a_exp*T_lim" color = colorant"yellow" begin
+        NVTX.@range "U=u+dt*a_exp*T_lim" begin
             @. U[i] = u + dt * a_exp[i, 1] * T_lim[1]
         end
-        NVTX.@range "lim!" color = colorant"yellow" begin
+        NVTX.@range "lim!" color = colorant"blue" begin
             lim!(U[i], p, t_exp, u)
         end
-        NVTX.@range "U+=dt*a_exp*T_exp" color = colorant"yellow" begin
+        NVTX.@range "U+=dt*a_exp*T_exp" begin
             @. U[i] += dt * a_exp[i, 1] * T_exp[1]
         end
-        NVTX.@range "dss!" color = colorant"yellow" begin
+        NVTX.@range "dss!" color = colorant"green" begin
             dss!(U[i], p, t_exp)
         end
     end
-    NVTX.@range "Stage 2" color = colorant"yellow" begin
-        NVTX.@range "temp = U[i]" color = colorant"yellow" begin
+    NVTX.@range "Stage 2" color = colorant"brown" begin
+        NVTX.@range "temp = U[i]" begin
             @. temp = U[i] # used in closures
         end
         let i = i
             implicit_equation_residual! =
                 (residual, Ui) -> begin
-                    NVTX.@range "T_imp!" color = colorant"yellow" begin
+                    NVTX.@range "T_imp!" color = colorant"pink" begin
                         T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
                     end
-                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" color = colorant"yellow" begin
+                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" begin
                         @. residual = temp + dt * a_imp[i, i] * residual - Ui
                     end
                 end
             implicit_equation_jacobian! =
                 (jacobian, Ui) -> begin
-                    NVTX.@range "Wfact" color = colorant"yellow" begin
+                    NVTX.@range "Wfact" color = colorant"orange" begin
                         T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
                     end
                 end
@@ -82,50 +82,50 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
             )
         end
 
-        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" color = colorant"yellow" begin
+        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" begin
             @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
         end
 
-        NVTX.@range "T_lim!" color = colorant"yellow" begin
+        NVTX.@range "T_lim!" color = colorant"red" begin
             T_lim!(T_lim[i], U[i], p, t_exp)
         end
-        NVTX.@range "T_exp!" color = colorant"yellow" begin
+        NVTX.@range "T_exp!" color = colorant"purple" begin
             T_exp!(T_exp[i], U[i], p, t_exp)
         end
 
         i = 3
         t_exp = t + dt * c_exp[i]
-        NVTX.@range "U=u+dt*..." color = colorant"yellow" begin
+        NVTX.@range "U=u+dt*..." begin
             @. U[i] = u + dt * a_exp[i, 1] * T_lim[1] + dt * a_exp[i, 2] * T_lim[2]
         end
-        NVTX.@range "lim!" color = colorant"yellow" begin
+        NVTX.@range "lim!" color = colorant"blue" begin
             lim!(U[i], p, t_exp, u)
         end
-        NVTX.@range "U=u+dt*..." color = colorant"yellow" begin
+        NVTX.@range "U=u+dt*..." begin
             @. U[i] += dt * a_exp[i, 1] * T_exp[1] + dt * a_exp[i, 2] * T_exp[2] + dt * a_imp[i, 2] * T_imp[2]
         end
-        NVTX.@range "dss!" color = colorant"yellow" begin
+        NVTX.@range "dss!" color = colorant"green" begin
             dss!(U[i], p, t_exp)
         end
     end
 
-    NVTX.@range "Stage 3" color = colorant"yellow" begin
-        NVTX.@range "temp = U[i]" color = colorant"yellow" begin
+    NVTX.@range "Stage 3" color = colorant"brown" begin
+        NVTX.@range "temp = U[i]" begin
             @. temp = U[i] # used in closures
         end
         let i = i
             implicit_equation_residual! =
                 (residual, Ui) -> begin
-                    NVTX.@range "T_imp!" color = colorant"yellow" begin
+                    NVTX.@range "T_imp!" color = colorant"pink" begin
                         T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
                     end
-                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" color = colorant"yellow" begin
+                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" begin
                         @. residual = temp + dt * a_imp[i, i] * residual - Ui
                     end
                 end
             implicit_equation_jacobian! =
                 (jacobian, Ui) -> begin
-                    NVTX.@range "Wfact" color = colorant"yellow" begin
+                    NVTX.@range "Wfact" color = colorant"orange" begin
                         T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
                     end
                 end
@@ -138,25 +138,25 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
             )
         end
 
-        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" color = colorant"yellow" begin
+        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" begin
             @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
         end
 
-        NVTX.@range "T_lim!" color = colorant"yellow" begin
+        NVTX.@range "T_lim!" color = colorant"red" begin
             T_lim!(T_lim[i], U[i], p, t_exp)
         end
-        NVTX.@range "T_exp!" color = colorant"yellow" begin
+        NVTX.@range "T_exp!" color = colorant"purple" begin
             T_exp!(T_exp[i], U[i], p, t_exp)
         end
         i = 4
         t_exp = t + dt
-        NVTX.@range "U=u+dt*..." color = colorant"yellow" begin
+        NVTX.@range "U=u+dt*..." begin
             @. U[i] = u + dt * a_exp[i, 1] * T_lim[1] + dt * a_exp[i, 2] * T_lim[2] + dt * a_exp[i, 3] * T_lim[3]
         end
-        NVTX.@range "lim!" color = colorant"yellow" begin
+        NVTX.@range "lim!" color = colorant"blue" begin
             lim!(U[i], p, t_exp, u)
         end
-        NVTX.@range "U+=dt*..." color = colorant"yellow" begin
+        NVTX.@range "U+=dt*..." begin
             @. U[i] +=
                 dt * a_exp[i, 1] * T_exp[1] +
                 dt * a_exp[i, 2] * T_exp[2] +
@@ -164,27 +164,27 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
                 dt * a_imp[i, 2] * T_imp[2] +
                 dt * a_imp[i, 3] * T_imp[3]
         end
-        NVTX.@range "dss!" color = colorant"yellow" begin
+        NVTX.@range "dss!" color = colorant"green" begin
             dss!(U[i], p, t_exp)
         end
     end
-    NVTX.@range "Stage 4" color = colorant"yellow" begin
-        NVTX.@range "temp = U[i]" color = colorant"yellow" begin
+    NVTX.@range "Stage 4" color = colorant"brown" begin
+        NVTX.@range "temp = U[i]" begin
             @. temp = U[i] # used in closures
         end
         let i = i
             implicit_equation_residual! =
                 (residual, Ui) -> begin
-                    NVTX.@range "T_imp!" color = colorant"yellow" begin
+                    NVTX.@range "T_imp!" color = colorant"pink" begin
                         T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
                     end
-                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" color = colorant"yellow" begin
+                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" begin
                         @. residual = temp + dt * a_imp[i, i] * residual - Ui
                     end
                 end
             implicit_equation_jacobian! =
                 (jacobian, Ui) -> begin
-                    NVTX.@range "Wfact" color = colorant"yellow" begin
+                    NVTX.@range "Wfact" color = colorant"orange" begin
                         T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
                     end
                 end
@@ -197,14 +197,14 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
             )
         end
 
-        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" color = colorant"yellow" begin
+        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" begin
             @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
         end
 
-        NVTX.@range "T_lim!" color = colorant"yellow" begin
+        NVTX.@range "T_lim!" color = colorant"red" begin
             T_lim!(T_lim[i], U[i], p, t_exp)
         end
-        NVTX.@range "T_exp!" color = colorant"yellow" begin
+        NVTX.@range "T_exp!" color = colorant"purple" begin
             T_exp!(T_exp[i], U[i], p, t_exp)
         end
 
@@ -212,13 +212,13 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
         i = -1
 
         t_final = t + dt
-        NVTX.@range "temp=u+dt*..." color = colorant"yellow" begin
+        NVTX.@range "temp=u+dt*..." begin
             @. temp = u + dt * b_exp[2] * T_lim[2] + dt * b_exp[3] * T_lim[3] + dt * b_exp[4] * T_lim[4]
         end
-        NVTX.@range "lim!" color = colorant"yellow" begin
+        NVTX.@range "lim!" color = colorant"blue" begin
             lim!(temp, p, t_final, u)
         end
-        NVTX.@range "u=temp+..." color = colorant"yellow" begin
+        NVTX.@range "u=temp+..." begin
             @. u =
                 temp +
                 dt * b_exp[2] * T_exp[2] +
@@ -228,7 +228,7 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
                 dt * b_imp[3] * T_imp[3] +
                 dt * b_imp[4] * T_imp[4]
         end
-        NVTX.@range "dss!" color = colorant"yellow" begin
+        NVTX.@range "dss!" color = colorant"green" begin
             dss!(u, p, t_final)
         end
     end

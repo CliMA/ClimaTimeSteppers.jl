d29870cda5817651ed99cb2f8090d0063c4150ce
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-09-21 07:18:51 -0700

    Rm NVTX calls

--- src/solvers/hard_coded_ars343.jl
+++ src/solvers/hard_coded_ars343.jl
@@ -11,227 +11,133 @@ function step_u!(integrator, cache::IMEXARKCache, ::ARS343)
     if !isnothing(newtons_method_cache)
         jacobian = newtons_method_cache.j
         if (!isnothing(jacobian)) && needs_update!(newtons_method.update_j, NewTimeStep(t))
-            NVTX.@range "Wfact" color = colorant"orange" begin
-                T_imp!.Wfact(jacobian, u, p, dt * γ, t)
-            end
+            T_imp!.Wfact(jacobian, u, p, dt * γ, t)
         end
     end
 
     s = 4
 
-    NVTX.@range "Stage 1" color = colorant"brown" begin
-        i::Int = 1
-        t_exp = t
-        NVTX.@range "U[i] = u" begin
-            @. U[i] = u
-        end
-        NVTX.@range "lim!" color = colorant"blue" begin
-            lim!(U[i], p, t_exp, u)
-        end
-        NVTX.@range "dss!" color = colorant"green" begin
-            dss!(U[i], p, t_exp)
-        end
-        NVTX.@range "T_lim!" color = colorant"red" begin
-            T_lim!(T_lim[i], U[i], p, t_exp)
-        end
-        NVTX.@range "T_exp!" color = colorant"purple" begin
-            T_exp!(T_exp[i], U[i], p, t_exp)
-        end
+    i::Int = 1
+    t_exp = t
+    @. U[i] = u
+    lim!(U[i], p, t_exp, u)
+    dss!(U[i], p, t_exp)
+    T_lim!(T_lim[i], U[i], p, t_exp)
+    T_exp!(T_exp[i], U[i], p, t_exp)
 
-        i = 2
-        t_exp = t + dt * c_exp[i]
-        NVTX.@range "U=u+dt*a_exp*T_lim" begin
-            @. U[i] = u + dt * a_exp[i, 1] * T_lim[1]
-        end
-        NVTX.@range "lim!" color = colorant"blue" begin
-            lim!(U[i], p, t_exp, u)
-        end
-        NVTX.@range "U+=dt*a_exp*T_exp" begin
-            @. U[i] += dt * a_exp[i, 1] * T_exp[1]
-        end
-        NVTX.@range "dss!" color = colorant"green" begin
-            dss!(U[i], p, t_exp)
-        end
-    end
-    NVTX.@range "Stage 2" color = colorant"brown" begin
-        NVTX.@range "temp = U[i]" begin
-            @. temp = U[i] # used in closures
-        end
-        let i = i
-            implicit_equation_residual! =
-                (residual, Ui) -> begin
-                    NVTX.@range "T_imp!" color = colorant"pink" begin
-                        T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
-                    end
-                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" begin
-                        @. residual = temp + dt * a_imp[i, i] * residual - Ui
-                    end
-                end
-            implicit_equation_jacobian! =
-                (jacobian, Ui) -> begin
-                    NVTX.@range "Wfact" color = colorant"orange" begin
-                        T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
-                    end
-                end
-            solve_newton!(
-                newtons_method,
-                newtons_method_cache,
-                U[i],
-                implicit_equation_residual!,
-                implicit_equation_jacobian!,
-            )
-        end
+    i = 2
+    t_exp = t + dt * c_exp[i]
+    @. U[i] = u + dt * a_exp[i, 1] * T_lim[1]
+    lim!(U[i], p, t_exp, u)
+    @. U[i] += dt * a_exp[i, 1] * T_exp[1]
+    dss!(U[i], p, t_exp)
 
-        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" begin
-            @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
+    @. temp = U[i] # used in closures
+    let i = i
+        implicit_equation_residual! = (residual, Ui) -> begin
+            T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
+            @. residual = temp + dt * a_imp[i, i] * residual - Ui
         end
-
-        NVTX.@range "T_lim!" color = colorant"red" begin
-            T_lim!(T_lim[i], U[i], p, t_exp)
-        end
-        NVTX.@range "T_exp!" color = colorant"purple" begin
-            T_exp!(T_exp[i], U[i], p, t_exp)
-        end
-
-        i = 3
-        t_exp = t + dt * c_exp[i]
-        NVTX.@range "U=u+dt*..." begin
-            @. U[i] = u + dt * a_exp[i, 1] * T_lim[1] + dt * a_exp[i, 2] * T_lim[2]
-        end
-        NVTX.@range "lim!" color = colorant"blue" begin
-            lim!(U[i], p, t_exp, u)
-        end
-        NVTX.@range "U=u+dt*..." begin
-            @. U[i] += dt * a_exp[i, 1] * T_exp[1] + dt * a_exp[i, 2] * T_exp[2] + dt * a_imp[i, 2] * T_imp[2]
-        end
-        NVTX.@range "dss!" color = colorant"green" begin
-            dss!(U[i], p, t_exp)
-        end
+        implicit_equation_jacobian! =
+            (jacobian, Ui) -> begin
+                T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
+            end
+        solve_newton!(
+            newtons_method,
+            newtons_method_cache,
+            U[i],
+            implicit_equation_residual!,
+            implicit_equation_jacobian!,
+        )
     end
 
-    NVTX.@range "Stage 3" color = colorant"brown" begin
-        NVTX.@range "temp = U[i]" begin
-            @. temp = U[i] # used in closures
-        end
-        let i = i
-            implicit_equation_residual! =
-                (residual, Ui) -> begin
-                    NVTX.@range "T_imp!" color = colorant"pink" begin
-                        T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
-                    end
-                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" begin
-                        @. residual = temp + dt * a_imp[i, i] * residual - Ui
-                    end
-                end
-            implicit_equation_jacobian! =
-                (jacobian, Ui) -> begin
-                    NVTX.@range "Wfact" color = colorant"orange" begin
-                        T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
-                    end
-                end
-            solve_newton!(
-                newtons_method,
-                newtons_method_cache,
-                U[i],
-                implicit_equation_residual!,
-                implicit_equation_jacobian!,
-            )
-        end
+    @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
 
-        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" begin
-            @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
-        end
+    T_lim!(T_lim[i], U[i], p, t_exp)
+    T_exp!(T_exp[i], U[i], p, t_exp)
 
-        NVTX.@range "T_lim!" color = colorant"red" begin
-            T_lim!(T_lim[i], U[i], p, t_exp)
+    i = 3
+    t_exp = t + dt * c_exp[i]
+    @. U[i] = u + dt * a_exp[i, 1] * T_lim[1] + dt * a_exp[i, 2] * T_lim[2]
+    lim!(U[i], p, t_exp, u)
+    @. U[i] += dt * a_exp[i, 1] * T_exp[1] + dt * a_exp[i, 2] * T_exp[2] + dt * a_imp[i, 2] * T_imp[2]
+    dss!(U[i], p, t_exp)
+
+    @. temp = U[i] # used in closures
+    let i = i
+        implicit_equation_residual! = (residual, Ui) -> begin
+            T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
+            @. residual = temp + dt * a_imp[i, i] * residual - Ui
         end
-        NVTX.@range "T_exp!" color = colorant"purple" begin
-            T_exp!(T_exp[i], U[i], p, t_exp)
-        end
-        i = 4
-        t_exp = t + dt
-        NVTX.@range "U=u+dt*..." begin
-            @. U[i] = u + dt * a_exp[i, 1] * T_lim[1] + dt * a_exp[i, 2] * T_lim[2] + dt * a_exp[i, 3] * T_lim[3]
-        end
-        NVTX.@range "lim!" color = colorant"blue" begin
-            lim!(U[i], p, t_exp, u)
-        end
-        NVTX.@range "U+=dt*..." begin
-            @. U[i] +=
-                dt * a_exp[i, 1] * T_exp[1] +
-                dt * a_exp[i, 2] * T_exp[2] +
-                dt * a_exp[i, 3] * T_exp[3] +
-                dt * a_imp[i, 2] * T_imp[2] +
-                dt * a_imp[i, 3] * T_imp[3]
-        end
-        NVTX.@range "dss!" color = colorant"green" begin
-            dss!(U[i], p, t_exp)
-        end
+        implicit_equation_jacobian! =
+            (jacobian, Ui) -> begin
+                T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
+            end
+        solve_newton!(
+            newtons_method,
+            newtons_method_cache,
+            U[i],
+            implicit_equation_residual!,
+            implicit_equation_jacobian!,
+        )
     end
-    NVTX.@range "Stage 4" color = colorant"brown" begin
-        NVTX.@range "temp = U[i]" begin
-            @. temp = U[i] # used in closures
-        end
-        let i = i
-            implicit_equation_residual! =
-                (residual, Ui) -> begin
-                    NVTX.@range "T_imp!" color = colorant"pink" begin
-                        T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
-                    end
-                    NVTX.@range "residual=temp+dt*a_imp*residual-Ui" begin
-                        @. residual = temp + dt * a_imp[i, i] * residual - Ui
-                    end
-                end
-            implicit_equation_jacobian! =
-                (jacobian, Ui) -> begin
-                    NVTX.@range "Wfact" color = colorant"orange" begin
-                        T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
-                    end
-                end
-            solve_newton!(
-                newtons_method,
-                newtons_method_cache,
-                U[i],
-                implicit_equation_residual!,
-                implicit_equation_jacobian!,
-            )
-        end
 
-        NVTX.@range "T_imp=(U-temp)/(dt*a_imp)" begin
-            @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
-        end
+    @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
 
-        NVTX.@range "T_lim!" color = colorant"red" begin
-            T_lim!(T_lim[i], U[i], p, t_exp)
-        end
-        NVTX.@range "T_exp!" color = colorant"purple" begin
-            T_exp!(T_exp[i], U[i], p, t_exp)
-        end
+    T_lim!(T_lim[i], U[i], p, t_exp)
+    T_exp!(T_exp[i], U[i], p, t_exp)
+    i = 4
+    t_exp = t + dt
+    @. U[i] = u + dt * a_exp[i, 1] * T_lim[1] + dt * a_exp[i, 2] * T_lim[2] + dt * a_exp[i, 3] * T_lim[3]
+    lim!(U[i], p, t_exp, u)
+    @. U[i] +=
+        dt * a_exp[i, 1] * T_exp[1] +
+        dt * a_exp[i, 2] * T_exp[2] +
+        dt * a_exp[i, 3] * T_exp[3] +
+        dt * a_imp[i, 2] * T_imp[2] +
+        dt * a_imp[i, 3] * T_imp[3]
+    dss!(U[i], p, t_exp)
 
-        # final
-        i = -1
-
-        t_final = t + dt
-        NVTX.@range "temp=u+dt*..." begin
-            @. temp = u + dt * b_exp[2] * T_lim[2] + dt * b_exp[3] * T_lim[3] + dt * b_exp[4] * T_lim[4]
+    @. temp = U[i] # used in closures
+    let i = i
+        implicit_equation_residual! = (residual, Ui) -> begin
+            T_imp!(residual, Ui, p, t + dt * c_imp[i]) #= t_imp =#
+            @. residual = temp + dt * a_imp[i, i] * residual - Ui
         end
-        NVTX.@range "lim!" color = colorant"blue" begin
-            lim!(temp, p, t_final, u)
-        end
-        NVTX.@range "u=temp+..." begin
-            @. u =
-                temp +
-                dt * b_exp[2] * T_exp[2] +
-                dt * b_exp[3] * T_exp[3] +
-                dt * b_exp[4] * T_exp[4] +
-                dt * b_imp[2] * T_imp[2] +
-                dt * b_imp[3] * T_imp[3] +
-                dt * b_imp[4] * T_imp[4]
-        end
-        NVTX.@range "dss!" color = colorant"green" begin
-            dss!(u, p, t_final)
-        end
+        implicit_equation_jacobian! =
+            (jacobian, Ui) -> begin
+                T_imp!.Wfact(jacobian, Ui, p, dt * a_imp[i, i], t + dt * c_imp[i]) #= t_imp =#
+            end
+        solve_newton!(
+            newtons_method,
+            newtons_method_cache,
+            U[i],
+            implicit_equation_residual!,
+            implicit_equation_jacobian!,
+        )
     end
+
+    @. T_imp[i] = (U[i] - temp) / (dt * a_imp[i, i])
+
+    T_lim!(T_lim[i], U[i], p, t_exp)
+    T_exp!(T_exp[i], U[i], p, t_exp)
+
+    # final
+    i = -1
+
+    t_final = t + dt
+    @. temp = u + dt * b_exp[2] * T_lim[2] + dt * b_exp[3] * T_lim[3] + dt * b_exp[4] * T_lim[4]
+    lim!(temp, p, t_final, u)
+    @. u =
+        temp +
+        dt * b_exp[2] * T_exp[2] +
+        dt * b_exp[3] * T_exp[3] +
+        dt * b_exp[4] * T_exp[4] +
+        dt * b_imp[2] * T_imp[2] +
+        dt * b_imp[3] * T_imp[3] +
+        dt * b_imp[4] * T_imp[4]
+    dss!(u, p, t_final)
+
     return u
 end
 

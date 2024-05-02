fda0fd4e5763ec16df043ad72347bccdfbcba8ac
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-05-02 14:59:53 -0400

    Update aqua tests

--- test/aqua.jl
+++ test/aqua.jl
@@ -3,12 +3,6 @@ using ClimaTimeSteppers
 using Aqua
 
 @testset "Aqua tests (performance)" begin
-    # This tests that we don't accidentally run into
-    # https://github.com/JuliaLang/julia/issues/29393
-    # Aqua.test_unbound_args(ClimaTimeSteppers)
-    ua = Aqua.detect_unbound_args_recursively(ClimaTimeSteppers)
-    @test length(ua) == 0
-
     # See: https://github.com/SciML/OrdinaryDiffEq.jl/issues/1750
     # Test that we're not introducing method ambiguities across deps
     ambs = Aqua.detect_ambiguities(ClimaTimeSteppers; recursive = true)
@@ -23,13 +17,8 @@ using Aqua
     @test length(ambs) == 0
 end
 
-@testset "Aqua tests (additional)" begin
-    Aqua.test_undefined_exports(ClimaTimeSteppers)
-    Aqua.test_stale_deps(ClimaTimeSteppers)
-    Aqua.test_deps_compat(ClimaTimeSteppers)
-    Aqua.test_project_extras(ClimaTimeSteppers)
-    Aqua.test_project_toml_formatting(ClimaTimeSteppers)
-    Aqua.test_piracy(ClimaTimeSteppers)
+@testset "Aqua tests (remaining)" begin
+    Aqua.test_all(ClimaTimeSteppers; ambiguities=false)
 end
 
 nothing

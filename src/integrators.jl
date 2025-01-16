ba912306833fb7e437f466599a6498b91063b3ad
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-01-15 16:11:56 -0800

    Remove check for number in tstops_and_saveat_heaps
    We do this because for long runs, you might want to change the type of
    time (e.g. ITime) which may not be a number.

--- src/integrators.jl
+++ src/integrators.jl
@@ -66,7 +66,7 @@ end
 
 
 # helper function for setting up min/max heaps for tstops and saveat
-function tstops_and_saveat_heaps(t0, tf, tstops, saveat)
+function tstops_and_saveat_heaps(t0, tf, tstops, saveat = [])
     FT = typeof(tf)
     ordering = tf > t0 ? DataStructures.FasterForward : DataStructures.FasterReverse
 
@@ -74,18 +74,9 @@ function tstops_and_saveat_heaps(t0, tf, tstops, saveat)
     tstops = [filter(t -> t0 < t < tf || tf < t < t0, tstops)..., tf]
     tstops = DataStructures.BinaryHeap{FT, ordering}(tstops)
 
-    if isnothing(saveat)
-        saveat = [t0, tf]
-    elseif saveat isa Number
-        saveat > zero(saveat) || error("saveat value must be positive")
-        saveat = tf > t0 ? saveat : -saveat
-        saveat = [t0:saveat:tf..., tf]
-    else
-        # We do not need to filter saveat like tstops because the saving
-        # callback will ignore any times that are not between t0 and tf.
-        saveat = collect(saveat)
-    end
-    saveat = DataStructures.BinaryHeap{FT, ordering}(saveat)
+    isnothing(saveat) && (saveat = [t0, tf])
+
+    saveat = DataStructures.BinaryHeap{FT, ordering}(collect(saveat))
 
     return tstops, saveat
 end

9ff67b0ae80d16ad7b948707516d256ff17a46f2
Dennis Yatunin <dyatun@gmail.com>
2025-01-31 17:41:19 -0800

    Clean up checks for convergence and limiters

--- docs/src/dev/limiter_summary.jl
+++ docs/src/dev/limiter_summary.jl
@@ -1,29 +1,114 @@
-#=
-julia --project=docs
-using Revise; include(joinpath("docs", "src", "dev", "limiter_summary.jl"))
+import Plots
+import JLD2
+using ClimaCorePlots
+using LinearAlgebra: norm
+using PrettyTables: pretty_table, ft_printf
 
-=#
-using ClimaTimeSteppers
-using Test
+title_str(name) = titlecase(replace(string(name), '_' => ' '))
 
-import ClimaTimeSteppers as CTS
-include(joinpath(pkgdir(ClimaTimeSteppers), "test", "problems.jl"))
-include(joinpath(@__DIR__, "compute_convergence.jl"))
-include(joinpath(@__DIR__, "..", "plotting_utils.jl"))
-using ClimaTimeSteppers
+# Generates the Table 1 from
+# "Optimization-based limiters for the spectral element method" by Guba et al.,
+# and also plots the values used to generate the table.
+function limiter_summary(sol_dicts, alg_strs)
+    table_rows = []
+    mkpath("output")
+    for alg_str in alg_strs
+        plots = []
+        plot_kwargs = (;
+            clims = (0, 1),
+            color = :diverging_rainbow_bgymr_45_85_c67_n256,
+            colorbar = false,
+            guide = "",
+            margin = 10Plots.px,
+        )
+        for use_limiter in (false, true), use_hyperdiffusion in (false, true)
+            solution = sol_dicts[alg_str, use_hyperdiffusion, use_limiter]
+            initial_q = solution[1].ρq ./ solution[1].ρ
+            final_q = solution[end].ρq ./ solution[end].ρ
+            names = propertynames(initial_q)
 
-let # Convergence
-    # NOTE: Some imperfections in the convergence order for SSPKnoth are to be
-    # expected because we are not using the exact Jacobian
-    vector_of_dicts = []
-    files = readdir()
-    filter!(x -> endswith(x, ".jld2"), files)
-    filter!(x -> startswith(basename(x), "limiter_"), files)
-    for f in files
-        push!(vector_of_dicts, JLD2.load_object(f))
+            if isempty(plots)
+                for name in names
+                    push!(plots, Plots.plot(initial_q.:($name); plot_kwargs..., title = title_str(name)))
+                end
+            end
+            for name in names
+                push!(plots, Plots.plot(final_q.:($name); plot_kwargs..., title = ""))
+            end
+
+            for name in names
+                ϕ₀ = initial_q.:($name)
+                ϕ = final_q.:($name)
+                Δϕ₀ = maximum(ϕ₀) - minimum(ϕ₀)
+                ϕ_error = ϕ .- ϕ₀
+                table_row = [
+                    alg_str;;
+                    string(use_limiter);;
+                    string(use_hyperdiffusion);;
+                    title_str(name);;
+                    max(0, -(minimum(ϕ) - minimum(ϕ₀)) / Δϕ₀);;
+                    max(0, (maximum(ϕ) - maximum(ϕ₀)) / Δϕ₀);;
+                    map(p -> norm(ϕ_error, p) / norm(ϕ₀, p), (1, 2, Inf))...
+                ]
+                push!(table_rows, table_row)
+            end
+        end
+        colorbar_plot = Plots.scatter(
+            [0];
+            plot_kwargs...,
+            colorbar = true,
+            framestyle = :none,
+            legend_position = :none,
+            margin = 0Plots.px,
+            markeralpha = 0,
+            zcolor = [0],
+        )
+        plot = Plots.plot(
+            plots...,
+            colorbar_plot;
+            layout = (Plots.@layout [Plots.grid(5, 3) a{0.1w}]),
+            plot_title = "Tracer specific humidity for $alg_str (Initial, \
+                          Final, Final w/ Hyperdiffusion, Final w/ Limiter, \
+                          Final w/ Hyperdiffusion & Limiter)",
+            size = (1600, 2000),
+        )
+        Plots.savefig(plot, joinpath("output", "limiter_summary_$alg_str.png"))
     end
-
-    sol_dicts = merge(vector_of_dicts...)
-    algorithm_names = map(x -> x[1], collect(keys(sol_dicts)))
-    limiter_summary(sol_dicts, algorithm_names)
+    table = pretty_table(
+        vcat(table_rows...);
+        header = [
+            "Algorithm",
+            "Limiter",
+            "Hyperdiffusion",
+            "Tracer Name",
+            "Max Undershoot",
+            "Max Overshoot",
+            "1-Norm Error",
+            "2-Norm Error",
+            "∞-Norm Error",
+        ],
+        crop = :none,
+        body_hlines = collect(3:3:(length(table_rows) - 1)),
+        formatters = ft_printf("%.4e"),
+    )
+    println(table)
 end
+
+limiter_results_filenames = filter(readdir("output"; join = true)) do filename
+    startswith(basename(filename), "limiter_") && endswith(filename, ".jld2")
+end
+limiter_results = merge(map(JLD2.load_object, limiter_results_filenames)...)
+alg_strs = unique(map(key -> key[1], collect(keys(limiter_results))))
+limiter_summary(limiter_results, alg_strs)
+
+#= TO ANALYZE LIMITERS WITH ARS343 AND SSP333:
+for alg_str in ("ARS343", "SSP333"), use_limiter in (false, true), use_hyperdiffusion in (false, true)
+    empty!(ARGS)
+    push!(ARGS, "--alg", alg_str)
+    push!(ARGS, "--use_limiter", string(use_limiter))
+    push!(ARGS, "--use_hyperdiffusion", string(use_hyperdiffusion))
+    @info "Running deformational flow with $alg_str, limiter = $use_limiter, hyperdif = $use_hyperdiffusion"
+    include("docs/src/dev/limiter_analysis.jl")
+end
+include("docs/src/dev/limiter_summary.jl")
+=#
--- docs/src/dev/report_gen_alg.jl
+++ docs/src/dev/report_gen_alg.jl
@@ -1,49 +1,9 @@
-#=
-julia --project=docs
-using Revise; include(joinpath("docs", "src", "dev", "report_gen_alg.jl"))
-
-Tested algs:
-
- - `SSP22Heuns`
- - `SSP33ShuOsher`
- - `RK4`
- - `ARK2GKC`
- - `ARS111`
- - `ARS121`
- - `ARS122`
- - `ARS222`
- - `ARS232`
- - `ARS233`
- - `ARS343`
- - `ARS443`
- - `SSP222`
- - `SSP322`
- - `SSP332`
- - `SSP333`
- - `SSP433`
- - `DBM453`
- - `HOMMEM1`
- - `IMKG232a`
- - `IMKG232b`
- - `IMKG242a`
- - `IMKG242b`
- - `IMKG243a`
- - `IMKG252a`
- - `IMKG252b`
- - `IMKG253a`
- - `IMKG253b`
- - `IMKG254a`
- - `IMKG254b`
- - `IMKG254c`
- - `IMKG342a`
- - `IMKG343a`
- - `SSPKnoth`
-=#
-using ClimaTimeSteppers
-using Test
-using InteractiveUtils: subtypes
-
 import ArgParse
+import JLD2
+
+include(joinpath(@__DIR__, "compute_convergence.jl"))
+include(joinpath(pkgdir(ClimaTimeSteppers), "test", "problems.jl"))
+
 function parse_commandline()
     s = ArgParse.ArgParseSettings()
     ArgParse.@add_arg_table s begin
@@ -55,55 +15,57 @@ function parse_commandline()
     parsed_args = ArgParse.parse_args(ARGS, s)
     return parsed_args
 end
+parsed_args = parse_commandline()
+alg_str = parsed_args["alg"]
+alg_name = getproperty(ClimaTimeSteppers, Symbol(alg_str))()
 
-ENV["GKSwstype"] = "nul" # avoid displaying plots
+convergence_results = Dict()
 
-include(joinpath(@__DIR__, "compute_convergence.jl"))
-include(joinpath(pkgdir(ClimaTimeSteppers), "test", "problems.jl"))
-import ClimaTimeSteppers as CTS
-all_subtypes(::Type{T}) where {T} = isabstracttype(T) ? vcat(all_subtypes.(subtypes(T))...) : [T]
+test_convergence!(convergence_results, alg_name, ark_analytic_nonlin_test_cts(Float64), 450)
+test_convergence!(convergence_results, alg_name, ark_analytic_sys_test_cts(Float64), 200)
+test_convergence!(
+    convergence_results,
+    alg_name,
+    ark_analytic_test_cts(Float64),
+    25000;
+    super_convergence_algorithm_names = (ARS121(),),
+)
+test_convergence!(convergence_results, alg_name, onewaycouple_mri_test_cts(Float64), 9000; high_order_sample_shifts = 3)
 
-let # Convergence
-    parsed_args = parse_commandline()
-    alg_name = getproperty(CTS, Symbol(parsed_args["alg"]))()
-    # NOTE: Some imperfections in the convergence order for SSPKnoth are to be
-    # expected because we are not using the exact Jacobian
+test_convergence!(
+    convergence_results,
+    alg_name,
+    climacore_1Dheat_test_cts(Float64),
+    200;
+    numerical_reference_algorithm_name = ARK548L2SA2(),
+)
+test_convergence!(
+    convergence_results,
+    alg_name,
+    climacore_1Dheat_test_implicit_cts(Float64),
+    200;
+    high_order_sample_shifts = 2,
+    numerical_reference_algorithm_name = ARK548L2SA2(),
+    broken_tests = (SSP22Heuns(), SSP33ShuOsher(), RK4(), IMKG253b(), IMKG254a(), IMKG254b(), IMKG343a()),
+) # This problem exceeds the CFL bounds of ERK methods, and it is unstable for several IMKG methods.
+test_convergence!(
+    convergence_results,
+    alg_name,
+    climacore_2Dheat_test_cts(Float64),
+    200;
+    numerical_reference_algorithm_name = ARK548L2SA2(),
+)
 
-    export_convergence_results(alg_name, ark_analytic_nonlin_test_cts(Float64), 200)
-    export_convergence_results(alg_name, ark_analytic_sys_test_cts(Float64), 400)
+mkpath("output")
+JLD2.save_object(joinpath("output", "convergence_$alg_str.jld2"), convergence_results)
 
-    export_convergence_results(alg_name, ark_analytic_test_cts(Float64), 40000; super_convergence = (ARS121(),))
-    export_convergence_results(alg_name, onewaycouple_mri_test_cts(Float64), 10000; num_steps_scaling_factor = 5)
-    export_convergence_results(
-        alg_name,
-        climacore_1Dheat_test_cts(Float64),
-        400;
-        num_steps_scaling_factor = 4,
-        numerical_reference_algorithm_name = ARS343(),
-    )
-
-    rosenbrock_schemes = filter(name -> name isa ClimaTimeSteppers.RosenbrockAlgorithmName, get_algorithm_names())
-    if any(x -> alg_name isa typeof(x), rosenbrock_schemes)
-        export_convergence_results(alg_name, climacore_1Dheat_test_implicit_cts(Float64), 400)
-    end
-    export_convergence_results(
-        alg_name,
-        climacore_2Dheat_test_cts(Float64),
-        600;
-        num_steps_scaling_factor = 4,
-        numerical_reference_algorithm_name = ARS343(),
-    )
-
-    # Unconstrained vs SSP results without limiters
-    parsed_args = parse_commandline()
-    alg_name = getproperty(CTS, Symbol(parsed_args["alg"]))()
-    ssp_schemes = all_subtypes(ClimaTimeSteppers.IMEXSSPRKAlgorithmName)
-    if any(x -> alg_name isa x, ssp_schemes)
-        test_unconstrained_vs_ssp_without_limiters(alg_name, ark_analytic_nonlin_test_cts(Float64), 200)
-        test_unconstrained_vs_ssp_without_limiters(alg_name, ark_analytic_sys_test_cts(Float64), 400)
-        test_unconstrained_vs_ssp_without_limiters(alg_name, ark_analytic_test_cts(Float64), 40000)
-        test_unconstrained_vs_ssp_without_limiters(alg_name, onewaycouple_mri_test_cts(Float64), 10000)
-        test_unconstrained_vs_ssp_without_limiters(alg_name, climacore_1Dheat_test_cts(Float64), 200)
-        test_unconstrained_vs_ssp_without_limiters(alg_name, climacore_2Dheat_test_cts(Float64), 200)
-    end
+# Unconstrained vs SSP tests
+if alg_name isa ClimaTimeSteppers.IMEXSSPRKAlgorithmName
+    test_unconstrained_vs_ssp_without_limiters(alg_name, ark_analytic_nonlin_test_cts(Float64), 450)
+    test_unconstrained_vs_ssp_without_limiters(alg_name, ark_analytic_sys_test_cts(Float64), 200)
+    test_unconstrained_vs_ssp_without_limiters(alg_name, ark_analytic_test_cts(Float64), 25000)
+    test_unconstrained_vs_ssp_without_limiters(alg_name, onewaycouple_mri_test_cts(Float64), 9000)
+    test_unconstrained_vs_ssp_without_limiters(alg_name, climacore_1Dheat_test_cts(Float64), 200)
+    test_unconstrained_vs_ssp_without_limiters(alg_name, climacore_1Dheat_test_implicit_cts(Float64), 200)
+    test_unconstrained_vs_ssp_without_limiters(alg_name, climacore_2Dheat_test_cts(Float64), 200)
 end

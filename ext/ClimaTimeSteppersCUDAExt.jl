d68d9bf3301cb260c7033fdecdfde6885ce11fbd
Tapio Schneider <tapio@caltech.edu>
2026-05-09 17:32:41 +0000

    Add code comments

--- ext/ClimaTimeSteppersCUDAExt.jl
+++ ext/ClimaTimeSteppersCUDAExt.jl
@@ -24,16 +24,20 @@ isolated compute stream to permit concurrent execution with standard Stage 1
 RHS evaluations on the GPU.
 """
 function ClimaTimeSteppers.async_Wfact!(T_imp!, jacobian, u::CUDA.CuArray, p, dtγ, t)
+    # Fork execution onto secondary compute stream if running on a GPU hardware backend
     if ClimaComms.device() isa ClimaComms.CUDADevice
         jac_stream, jac_event = get_jac_resources()
 
+        # Execute Wfact! inside the alternate stream, parallel with default Stage 1 tendencies
         CUDA.stream!(jac_stream) do
             T_imp!.Wfact(jacobian, u, p, dtγ, t)
         end
 
+        # Record non-blocking event so default stream knows when Jacobian has finished assembling
         CUDA.record(jac_event, jac_stream)
         return jac_event
     else
+        # Synchronous CPU fallback fallback
         T_imp!.Wfact(jacobian, u, p, dtγ, t)
         return nothing
     end
